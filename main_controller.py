7de4add7429a7c800c25db94b4b331f9e4baa0e6
Celia Benisty <celia.benistys@gmail.com>
2021-07-02 15:44:01 +0200

    RPi - Controller v2

--- main_controller.py
+++ main_controller.py
@@ -1,5 +1,6 @@
 import time
 import RPi.GPIO as GPIO
+from time import strftime
 
 def printLog(msgLog):
 	print(msgLog)
@@ -18,6 +19,8 @@ GPIO.setup(RX, GPIO.IN)
 GPIO.setup(TX, GPIO.OUT)
 GPIO.setup(PX, GPIO.IN)
 
+start = -1
+
 GPIO.output(TX,1)
 
 #affichage lors de tests
@@ -27,14 +30,25 @@ try:
 	while True:
 		#Sensor reading, 0 = detection
 		if GPIO.input(PX) == 0:
-                        print("Capteur de proximite detecte")
+            start = time.time()
+            print("Capteur de proximite detecte")
 			GPIO.output(TX, 0)
 			time.sleep(0.1)
 			GPIO.output(TX, 1)
-		if GPIO.input(RX):
-			messageHandler("Sound detected")
-			
-		time.sleep(0.1) #wait for arduino to answer
+        else if ( GPIO.input(PX) == 1 & start != -1 ):
+            end = time.time()
+            fichier = open("log.txt", "a")
+            fichier.write("\nPrésence détectée pendant une durée de " end - start " secondes, le " strftime(%Y-%m-%d, datetime.now())" à " strftime(%H:%M:%S, datetime.now())
+            fichier.close()
+        if GPIO.input(RX) == 0:
+            messageHandler("Sound detected")
+            start = time.time()
+        else if ( GPIO.input(RX) == 1 & start != -1 ):
+            end = time.time()
+            fichier = open("log.txt", "a")
+            fichier.write("\nBruit détecté pendant une durée de " end - start " secondes, le " strftime(%Y-%m-%d, datetime.now())" à " strftime(%H:%M:%S, datetime.now())
+            fichier.close()
+            time.sleep(0.1) #wait for arduino to answer
 		
 except KeyboardInterrupt:
 	print("KeyboardInterrupt has been caught.")
