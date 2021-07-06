107919b66a211d882dc5bd4d4d8bf575141fbf26
Poste1 <kradre@hotmail.fr>
2021-07-06 14:53:27 +0200

    Auto log - Script d'automatisation de l'enregistrement des logs sur git

--- auto_log.sh
+++ auto_log.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+#WARNING : PATH du dossier git en absolu !
+git_path="/home/pi/Desktop/projet/Systeme-de-detection"
+#deplacement dans le dossier git
+cd $git_path
+
 #test presence du fichier
 if [ ! -f log.txt ]
 then 
     exit 0 #rien a faire 
 fi
-#deplacement dans la branche de commit ?
 
-#test modification du fichier ?
+#commit des logs uniquement sans affichage
+git add log.txt
+git commit -m "LOG AUTO COMMIT - $(date '+%Y-%m-%d %H:%M:%S') " log.txt >/dev/null
 
-#test commit des logs
 
-#test push des logs
-
+#push des logs sans affichage
+git push >/dev/null
 
