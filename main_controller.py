b9abcdd7ca16801bdf4c0736398e9f46186b6417
Poste1 <kradre@hotmail.fr>
2021-07-06 17:07:45 +0200

    RPi - controller_fonction v4

--- main_controller.py
+++ main_controller.py
@@ -8,40 +8,42 @@ import datetime
 
 # Declaration des fonctions
 
-
-def ecriture_log(detection):
+def ecriture_log(detection,start,end):
 
     log = open("log.txt", "a")
     log.write("\n" + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S ") + str(detection) + " pendant une duree de " + str(round(float(end - start),2)) + " secondes")
-    derniere_ligne = log.readlines()[-1]
+   # derniere_ligne = log.readlines()[-1]
     log.close()
-    return derniere_ligne
+  #  return derniere_ligne
 
-def capteur_presence(GPIO.input(PX)):
+def capteur_presence(GPIO_PX,start):
     #Sensor reading, 0 = detection
-    if GPIO.input(PX) == 0:
+    if GPIO_PX == 0:
         if start == -1:
             start = time.time()
         print("Capteur de proximite detecte")
         GPIO.output(TX, 0)
         time.sleep(0.1)
-        
-    elif ( GPIO.input(PX) == 1 and start != -1 ):
-        GPIO.output(TX, 1)
+        GPIO.output(TX, 1) 
+    elif ( GPIO_PX == 1 and start != -1 ):
         end = time.time()
-        ecriture_log('Presence detectee')
-        start = -1
-    return GPIO.output(TX)
-        
-def capteur_son (GPIO.input(RX)):
+        ecriture_log('Presence detectee',start,end)
+	print("capteur plus detecte")
+	start = -1
+    return start 
+
+def capteur_son (GPIO_RX,start):
     #Sensor reading, 1 = detection
-    if GPIO.input(RX) == 1:
+    if GPIO_RX == 1:
         if start == -1:
             start = time.time()
-    elif ( GPIO.input(RX) == 0 and start != -1 ):
+	print("son detecte")
+	return start
+    elif ( GPIO_RX == 0 and start != -1 ):
         end = time.time()
-        ecriture_log('Bruit detecte')
-        start = -1
+        ecriture_log('Bruit detecte',start,end)
+	start = -1
+    return start
 
 def printLog(msgLog):
 	print(msgLog)
@@ -70,10 +72,10 @@ print('Running. Press CTRL-C to exit.')
 
 try:
 	while True:
-        capteur_presence(GPIO.input(PX))
-        
-        capteur_son(GPIO.input(RX))
-                        
+
+		start =	capteur_presence(GPIO.input(PX),start)
+       		 
+		start = capteur_son(GPIO.input(RX),start)
 		
 except KeyboardInterrupt:
 	print("KeyboardInterrupt has been caught.")
