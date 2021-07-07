fe9b5e5ac8834166441febf2138e602acb7dec67
Adrien C <kradare3@gmail.com>
2021-07-07 14:40:01 +0200

    RPi - correction indentation tab/space

--- test_main_controller.py
+++ test_main_controller.py
@@ -12,15 +12,15 @@ class TestController(unittest.TestCase):
     
     def test_capteur_presence(self):
         self.assertNotEqual(capteur_presence(0, -1),-1)
-	self.assertEqual(capteur_presence(1, -1), -1)
+        self.assertEqual(capteur_presence(1, -1), -1)
         self.assertEqual(capteur_presence(1, 1),-1)
-	self.assertNotEqual(capteur_presence(0, 1),-1)
+        self.assertNotEqual(capteur_presence(0, 1),-1)
     
     def test_capteur_son(self):
         self.assertNotEqual(capteur_son(1, -1),-1)
-	self.assertEqual(capteur_son(0, -1),-1)
+        self.assertEqual(capteur_son(0, -1),-1)
         self.assertEqual(capteur_son(0, 1),-1)
-	self.assertNotEqual(capteur_son(1, 1),-1)
+        self.assertNotEqual(capteur_son(1, 1),-1)
 
 if __name__ == '__main__':
 
